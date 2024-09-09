124b0e6f4a413b78d06e8fa0086ea17ac385a5a3
achuz12345 <aswathy4h@gmail.com>
2024-09-09 19:48:36 +0530

    Forth commit

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,8 @@
 from django.db import models
+from django.utils import timezone
 
 class Post(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=255)
     content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.title
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)  # This ensures the field updates automatically
